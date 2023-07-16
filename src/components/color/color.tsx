d7190b35864ce9e92e71df68b32d673641a0665c
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2023-07-17 03:03:33 +0500

    fix: color component add key prop

--- src/components/color/color.tsx
+++ src/components/color/color.tsx
@@ -11,7 +11,11 @@ const Color: React.FC<ColorProps> = ({ children }) => (
   <div className={cls.wrapper}>
     <div className={cls.container}>
       {COLOR_VARIABLE.map(item => (
-        <div className={cls.card} style={{ background: `var(${item})` }}>
+        <div
+          key={item}
+          className={cls.card}
+          style={{ background: `var(${item})` }}
+        >
           {item}:&nbsp;
           {getComputedStyle(document.documentElement).getPropertyValue(item)}
         </div>
