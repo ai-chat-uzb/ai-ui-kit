f9758b90a9a072cf0c12f39c3f6774aa8c3ac3ba
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2023-07-16 12:35:27 +0500

    feat: version 1.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -64,7 +64,7 @@ export default [
     ]
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/esm/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
     external: [/\.(css|less|scss)$/] //👈 new
