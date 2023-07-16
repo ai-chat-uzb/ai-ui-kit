dda59f8b67b85ea8eec1e8931960a313a217d94d
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2023-07-16 23:46:31 +0500

    feat: icons components

--- src/components/icon/list/index.ts
+++ src/components/icon/list/index.ts
@@ -1,6 +1,7 @@
-export { default as padlock } from './padlock';
+export { default as a } from './a';
 export { default as google } from './google';
 export { default as hamburger } from './hamburger';
 export { default as hide } from './hide';
 export { default as installmentPaid } from './installmentPaid';
 export { default as internet } from './internet';
+export { default as padlock } from './padlock';
