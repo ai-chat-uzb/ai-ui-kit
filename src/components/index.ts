bbe17f2e76c0f1d0fbeaf39b015fb44a4c617157
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2023-07-18 00:04:39 +0500

    fix: tabs component add project

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Alert } from './alert/alert';
 export { default as Button } from './button/button';
 export { default as Color } from './color/color';
 export { default as Icons } from './icon/icon';
+export { default as Tabs } from './tabs/tabs';
