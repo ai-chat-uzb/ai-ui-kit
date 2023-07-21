fdd34c8f058e8abce1883eed520114536db92a53
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2023-07-21 16:47:10 +0500

    feat: bug fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,11 @@
 export { default as Alert } from './alert/alert';
-export { default as Avatar } from './avatar/avatar';
 export { default as Button } from './button/button';
 export { default as Color } from './color/color';
 export { default as Divider } from './divider/divider';
-export { default as Dropdown } from './dropdown/dropdown';
 export { default as Icons } from './icon/icon';
 export { default as Input } from './input/input';
 export { default as Modal } from './modal/modal';
 export { default as Password } from './password/password';
 export { default as Tabs } from './tabs/tabs';
-export { default as Toast } from './toast/toast';
+export { default as toast } from './toast/toast';
 export { default as Typography } from './typography/typography';
-export { default as UserCard } from './user-card/user-card';
