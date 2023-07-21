c675c323c85b473b6dd72b56b0d7a3e4b407bf4f
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2023-07-21 16:28:17 +0500

    feat: update avatar

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import cx from 'classnames';
 
-import cls from './avatar.module.scss';
+import avatar from 'assets/images/icons/avatar.svg';
 
-const avatar: string = require('../../assets/images/icons/avatar.svg').default;
+import cls from './avatar.module.scss';
 
 export interface AvatarProps {
   avatarUrl?: string;
@@ -13,7 +13,7 @@ export interface AvatarProps {
 
 const Avatar: React.FC<AvatarProps> = ({ status, className, avatarUrl }) => (
   <div className={cx(cls.wrapper, className && className)}>
-    <img className={cls.user} src={avatarUrl || avatar} alt="" />
+    <img className={cls.user} src={avatarUrl || avatar} alt="img not found" />
     {status && (
       <div className={cls.status}>
         <div className={cx(cls.shape, cls[`${status}`])}></div>
